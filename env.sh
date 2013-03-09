a4a25cac1f6bca2f39b46f1d3a5dca1047ba7c91
Christopher Kim <christopherjkim@gmail.com>
2013-03-09 01:33:53 -0500

    Changed startup script so sizzbot restarts itself

---
+++ env.sh
@@ -0,0 +1,13 @@
+export PATH="node_modules/.bin:node_modules/hubot/node_modules/.bin:$PATH"
+export PORT=8012
+export HUBOT_IRC_SERVER="irc.gamesurge.net"
+export HUBOT_IRC_ROOMS="#SizzlingChat"
+export HUBOT_IRC_NICK="SizzBot"
+export HUBOT_IRC_NICKSERV_USERNAME="SizzBot"
+export HUBOT_IRC_UNFLOOD="true"
+# export HUBOT_IRC_DEBUG="true"
+# export HUBOT_IRC_PORT=6767
+# export HUBOT_IRC_PASSWORD="..."
+# export HUBOT_IRC_NICKSERV_PASSWORD="..."
+# export HUBOT_IRC_SERVER_FAKE_SSL="true"
+# export HUBOT_IRC_USESSL="true"
--- sizzbot.sh
+++ sizzbot.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
+trap 'exit' SIGINT
 
-npm install
-export PATH="node_modules/.bin:node_modules/hubot/node_modules/.bin:$PATH"
-export PORT=8012
-export HUBOT_IRC_SERVER="irc.gamesurge.net"
-export HUBOT_IRC_ROOMS="#SizzlingChat"
-export HUBOT_IRC_NICK="SizzBot"
-export HUBOT_IRC_NICKSERV_USERNAME="SizzBot"
-export HUBOT_IRC_UNFLOOD="true"
-# export HUBOT_IRC_DEBUG="true"
-# export HUBOT_IRC_PORT=6767
-# export HUBOT_IRC_PASSWORD="..."
-# export HUBOT_IRC_NICKSERV_PASSWORD="..."
-# export HUBOT_IRC_SERVER_FAKE_SSL="true"
-# export HUBOT_IRC_USESSL="true"
+while true
+do
+  npm install
 
-source ./secrets.sh
+  source ./env.sh
+  source ./secrets.sh
 
-node_modules/.bin/hubot \
-                  -a irc \
-                  -n "SizzBot" \
-                  -r "customscripts"
+  node_modules/.bin/hubot \
+                    -a irc \
+                    -n "SizzBot" \
+                    -r "customscripts"
+  if [ $? -eq 42 ]; then
+    exit
+  fi
 
+done
