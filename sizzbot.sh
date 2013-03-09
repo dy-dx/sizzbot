6a1e8695eda326a10866239e8a1e925b80ab2272
Christopher Kim <christopherjkim@gmail.com>
2013-03-08 22:04:09 -0500

    Source secrets.sh instead.

--- sizzbot.sh
+++ sizzbot.sh
@@ -6,18 +6,16 @@ export PORT=8012
 export HUBOT_IRC_SERVER="irc.gamesurge.net"
 export HUBOT_IRC_ROOMS="#SizzlingChat"
 export HUBOT_IRC_NICK="SizzBot"
+export HUBOT_IRC_NICKSERV_USERNAME="SizzBot"
 export HUBOT_IRC_UNFLOOD="true"
 # export HUBOT_IRC_DEBUG="true"
-
-# export HUBOT_IRC_NICK="..."
 # export HUBOT_IRC_PORT=6767
 # export HUBOT_IRC_PASSWORD="..."
 # export HUBOT_IRC_NICKSERV_PASSWORD="..."
 # export HUBOT_IRC_SERVER_FAKE_SSL="true"
 # export HUBOT_IRC_USESSL="true"
 
-export WORDNIK_API_KEY=""
-export HUBOT_WOLFRAM_APPID=""
+source ./secrets.sh
 
 node_modules/.bin/hubot \
                   -a irc \
